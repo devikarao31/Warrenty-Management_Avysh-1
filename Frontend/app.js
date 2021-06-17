b24c3994b3b58fbaadb3a6694735c7030397898c
devikarao31 <68768908+devikarao31@users.noreply.github.com>
2021-06-17 09:33:41 +0530

    Update app.js

--- Frontend/app.js
+++ Frontend/app.js
@@ -5,9 +5,31 @@ const mongoose = require("mongoose");
 const app = express();
 
 app.use(bodyparser.urlencoded({ extended: true }));
-app.engine("handlebars", exphbs());
+
+
 app.set("view engine", "handlebars");
-app.
+
+app.engine("handlebars", exphbs({
+  extname:'handlebars',
+  defaultLayout: 'index',
+  layoutsDir:__dirname+'/views/layouts',
+  partialsDir:__dirname+'/views/partials'
+}));
+
+app.get('/footer/:id',(req,res)=>{
+  
+})
+
+app.get('/footer',(req,res)=>{
+  res.render("footer")
+})
+
+app.get("/", (req,res)=>{
+  res.render("main")
+});
+
+
 app.listen(3000, function () {
   console.log("Server running on port 3000");
 });
+
